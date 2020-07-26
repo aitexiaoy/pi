99d76e35079863df47723595093998c48344a23f
杨鹏 <yangpeng@taoacat-2.local>
2020-07-26 18:41:06 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const Koa = require('koa');
+const rpio  =  require('rpio');
+const app = new Koa();
+const router = require('koa-router')();
+const koaBody = require('koa-body');
+app.use(require('koa-static')(__dirname + '/static/'));
+
+function startIo(){
+    rpio.write(16, rpio.HIGH);
+}
+
+function stopIo(){
+    rpio.write(16, rpio.LOW);
+}
+
+router.post('/startwater', koaBody(),
+    (ctx) => {
+        console.log(ctx.request.body);
+        // => POST body
+
+        startIo()
+        ctx.body = {
+            returnCode: "0", //默认表示成功，其他不成功的字段自己可以确定	是
+            returnErrorSolution: '',//	出错时解决办法的描述信息	否
+            returnMessage: '执行成功',//	String	返回执行成功的描述信息	否
+            returnValue: {
+                "reply": "已经开始浇花了",
+                "resultType": "RESULT",
+                "executeCode": "SUCCESS",
+                "msgInfo": ""
+            },//
+        };
+    }
+);
+
+router.post('/stopwater', koaBody(),
+    (ctx) => {
+        console.log(ctx.request.body);
+        // => POST body
+        stopIo()
+        ctx.body = {
+            returnCode: "0", //默认表示成功，其他不成功的字段自己可以确定	是
+            returnErrorSolution: '',//	出错时解决办法的描述信息	否
+            returnMessage: '执行成功',//	String	返回执行成功的描述信息	否
+            returnValue: {
+                "reply": "停止浇花",
+                "resultType": "RESULT",
+                "executeCode": "SUCCESS",
+                "msgInfo": ""
+            },//
+        };
+    }
+);
+
+
+app.use(router.routes());
+
+app.listen(3000);
+
+console.log('开启服务成功....')
